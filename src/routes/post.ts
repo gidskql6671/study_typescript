673dc2172fcce4cd131c9c59c8fb985fcfd5cf1c
김동환 <gidskql6671@gmail.com>
2021-06-09 11:44:10 +0000

    postService를 사용하여, post 데이터를 받아와 response로 돌려줌

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import { logger } from '../configs';
+import { createPost, getPost } from '../services';
+
 
 const router: express.Router = express.Router();
-
 
 
 router.get('/', (req: express.Request, res: express.Response) => {
 	logger.info("Board Router");
 	
-	res.send("Board!");
+	getPost({})
+	.then(data => {
+		res.send(data);
+	})
+	.catch(err => res.send(err));
 })
 
 
