bb263456de51b71b6b71b01df05ee05162b42296
김동환 <gidskql6671@gmail.com>
2021-06-13 00:28:31 +0000

    post create router 제작

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -7,14 +7,29 @@ const router: express.Router = express.Router();
 
 
 router.get('/', (req: express.Request, res: express.Response) => {
-	logger.info("Board Router");
+	logger.info("GET: ", req.path);
 	
 	postService.getPost({})
-	.then(data => {
-		res.send(data);
-	})
+	.then(posts => res.render('post/index', { posts }))
 	.catch(err => res.send(err));
 })
 
+router.get('/write', (req: express.Request, res: express.Response) => {
+	logger.info("GET: ", req.path);
+	
+	res.render('post/write');
+})
+
+router.post('/', (req, res) => {
+	const data = {
+		...req.body,
+		reg_id: "test_id",
+		reg_name: "test_name"
+	}
+	
+	postService.createPost(data)
+	.then(id => res.redirect('/post'))
+	.catch(err => res.send(err));
+})
 
 export default router;
